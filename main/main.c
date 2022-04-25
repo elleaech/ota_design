c470fde2a13ef5cd9fd263c50a9a658abbe315cf
elleaech <lohan.chuan123@gmail.com>
2022-04-25 20:53:44 -0300

    Logs added

--- main/main.c
+++ main/main.c
@@ -1,4 +1,5 @@
 #include "ota_data.h"
+#include "ota_log.h"
 #include "ota.h"
 
 #include <stdio.h>
@@ -9,6 +10,7 @@ static int32_t lb_set_params_(NovusOTA *ota_data);
 /* ---------------------------------------------------------------------- */
 void app_main(void)
 {
+    lb_ota_log_info(__FUNCTION__, "<%s>\n", __FUNCTION__);
     int rc = OTA_OK;
 
     NovusOTA *ota_data = NULL;
@@ -17,15 +19,19 @@ void app_main(void)
 
     if (OTA_OK == rc && NULL != ota_data)
     {
+        lb_ota_log_info(__FUNCTION__, "INIT SUCCESSFULL\n");
         rc = lb_set_params_(ota_data);
 
         if (OTA_OK == rc)
         {
+            lb_ota_log_info(__FUNCTION__, "<%s>\n", "lb_ota_update_firmware");
             lb_ota_update_firmware(ota_data);
+            lb_ota_log_info(__FUNCTION__, "</%s>\n", "lb_ota_update_firmware");
         }
 
         lb_ota_destroy_params(&ota_data);
     }
+    lb_ota_log_info(__FUNCTION__, "</%s>\n", __FUNCTION__);
 }
 
 /* ---------------------------------------------------------------------- */
@@ -46,5 +52,5 @@ lb_set_params_(NovusOTA *ota_data)
         rc = lb_ota_set_ssl_data(ota_data, NULL, 0);
     }
 
-    return rc;
+    return OTA_OK;
 }
